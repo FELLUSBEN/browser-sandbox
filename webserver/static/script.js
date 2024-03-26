fe6570de4c5050578a09e78afe828a0ea2d98e15
Ben Fellus <50238518+FELLUSBEN@users.noreply.github.com>
2024-03-26 11:46:12 +0200

    added download sidebar

---
+++ webserver/static/script.js
@@ -0,0 +1,11 @@
+document.getElementById("togglePanelBtn").onclick = function() {
+    var sidePanel = document.getElementById("sidePanel");
+    var mainContent = document.getElementById("mainContent");
+    if (sidePanel.style.width === "15%" || sidePanel.style.width === "") {
+        sidePanel.style.width = "0";
+        mainContent.style.marginLeft = "0"
+    } else {
+        sidePanel.style.width = "15%"; 
+        mainContent.style.marginLeft = "15%"
+    }
+};
