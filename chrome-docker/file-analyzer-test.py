177fd894a89119f48aab428d0440b1d50db39ed4
Ben Fellus <50238518+FELLUSBEN@users.noreply.github.com>
2024-04-26 12:34:49 +0300

    fixed some bugs in the container creation

--- chrome-docker/file-analyzer-test.py
+++ chrome-docker/file-analyzer-test.py
@@ -5,14 +5,17 @@ from watchdog.events import FileSystemEvent, FileSystemEventHandler
 import time
 #from static import yaraengine
 
+
 class Watcher(FileSystemEventHandler):
     def on_created(self, event: FileSystemEvent) -> None:
         #print(f"new file/dir created : {event.src_path}")
         if "crdownload" in event.src_path.split('/')[-1] or "com.google.Chrome" in event.src_path.split('/')[-1]:
-            return
+            return        
         root = tk.Tk()
         root.withdraw()
         messagebox.showerror("Downloaded File!", f"You downloaded {event.src_path.split('/')[-1]}!\n")
+        root.update_idletasks()
+        root.update()
         #yaraengine.CheckFile(event.src_path)
 
 
