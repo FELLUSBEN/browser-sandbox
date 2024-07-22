45ea857b52b3e47ba9b38b306b4095c9eaaeed1d
Ben Fellus <50238518+FELLUSBEN@users.noreply.github.com>
2024-07-22 20:21:46 +0300

    added dynamic screen resolution

--- webserver/static/script.js
+++ webserver/static/script.js
@@ -1,7 +1,7 @@
 document.getElementById("togglePanelBtn").onclick = function() {
     var sidePanel = document.getElementById("sidePanel");
     var mainContent = document.getElementById("mainContent");
-    if (sidePanel.style.width === "15%" || sidePanel.style.width === "") {
+    if (sidePanel.style.width === "15%" ) {
         sidePanel.style.width = "0";
         mainContent.style.marginLeft = "0"
     } else {
