910ffbfca4107a11a4bd89bd53f976187c6ded04
razi chai <razi.chai@gmail.com>
2024-10-04 12:01:07 +0300

    final sigma for project3

--- antivirus/sigma-engine(depricated)/pySigma-backend-pySigma-backend-bash/tests/test_pipelines_pySigma-backend-bash.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from sigma.backends.bash import bashBackend
-# from sigma.pipelines.bash import # TODO: import pipeline functions
-
-# TODO: import tests for all implemented pipelines and contained transformations
